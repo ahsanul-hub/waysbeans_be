bd1e788e1a2530048d77cf5f6bc12aef639ed188
ahsanul-hub <aldi.madridista.am@gmail.com>
2022-06-20 22:11:02 +0700

    update io server

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: process.env.CLIENT_URL || "http://localhost:3000",
+    origin: "https://waysbeans-ahsanul.netlify.app" || "http://localhost:3000",
   },
 });
 
